4851f29be3339ccecb534e1968a7978496667e10
haley-r <haleyryan77@gmail.com>
2020-02-16 16:03:54 -0600

    add tasks from input

--- server/modules/routes/tasks.router.js
+++ server/modules/routes/tasks.router.js
@@ -4,7 +4,29 @@ const router = express.Router();
 const pool = require('../pool');
 //routes
 //get
+router.get('/', (req,res)=>{
+    console.log('in /tasks GET');
+    //run this query on 'pool' db to get all tasks
+    let query = 'SELECT * FROM tasks';
+    pool.query(query).then((results)=>{
+        res.send(results.rows);
+    }).catch( (err)=>{
+        console.log(err);
+        res.sendStatus(500);
+    })//end query
+})//end GET
 //post
+router.post('/', (req, res) => {
+    console.log('in /tasks POST with ', req.body);
+    //run this query on 'pool' db to get all tasks
+    let query = 'INSERT INTO tasks ("description") VALUES ($1)';
+    pool.query(query, [req.body.description]).then((results) => {
+        res.sendStatus(201);
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    })//end query
+})//end POST
 //delete
 //put
 //exports
---
+++ server/public/scripts/client.js
@@ -0,0 +1,64 @@
+$(document).ready(onReady);
+
+function onReady(){
+    refreshTasks();
+    //attach click listeners
+    $('#addTaskButton').on('click', addTask);
+    $('#task-list').on('click', '.completeButton', completeTask);
+    $('#task-list').on('click', '.deleteButton', deleteTask);
+}
+
+function refreshTasks(){
+    console.log('in refreshTasks');
+    //get tasks as array of objects from db
+    $.ajax({
+        type: 'GET',
+        url: '/tasks'
+    }).then( function (response){
+        console.log('back from /tasks GET');
+        //target and empty the ul
+        let list = $('#task-list');
+        list.empty();
+        //loop through array and append as <li> to dom
+        for (task of response){
+            list.append(`
+            <li class="completed-${task.completed} data-id="${task.id}">
+            ${task.description}
+            <button class="completeButton">done!</button>
+            <button class="deleteButton">remove</button>
+            </li>
+            `)
+        }
+    }).catch( function (err){
+        console.log(err);
+        alert('problem refreshing tasks');
+    })//end AJAX
+}//end refreshTasks
+
+function addTask(){
+    console.log('in addTask');
+    //bundle inputs into object
+    let newTask = {
+        description: $('#descriptionIn').val()
+    }
+    //post request
+    $.ajax({
+        type: 'POST',
+        url: '/tasks',
+        data: newTask
+    }).then( function (response){
+        console.log('back from POST with:', response);
+        //update display
+        refreshTasks();
+    }).catch( function (err){
+        alert('unable to add task');
+        console.log(err);
+    })
+}
+
+function completeTask(){
+    console.log("in completeTask");
+}
+function deleteTask() {
+    console.log("in deleteTask");
+}
